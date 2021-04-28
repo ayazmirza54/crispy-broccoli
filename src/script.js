b753c9a917f2dd352d9ef50c0f7f1e7f43baf683
Ayaz Mirza <ayazmirza54@gmail.com>
2021-04-28 10:08:44 +0530

    a commit

--- src/script.js
+++ src/script.js
@@ -1,9 +1,9 @@
 // conventional method of selecting all paragraphs 
 //and changing their color property to blue
-var div = document.getElementsByTagName("div.container-flex");
-for (var i = 0; i < div.length; i++) {
-  var div = div.item(i);
-  div.style.setProperty("color", "#35598f", null);
+// var div = document.getElementsByTagName("div.container-flex");
+// for (var i = 0; i < div.length; i++) {
+//   var div = div.item(i);
+//   div.style.setProperty("color", "#35598f", null);
 }
-//D3 way of doing the same
-d3.selectAll("p").style("color", "#35598f");
+// //D3 way of doing the same
+// d3.selectAll("p").style("color", "#35598f");
